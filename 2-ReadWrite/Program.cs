69c84f0fd2b1b8b5525f5bf7f559abc37c30642b
fellyka <fellyka@felika.felika>
2023-05-02 04:35:50 +0200

    Introduction

---
+++ 2-ReadWrite/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_ReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            string fullName = firstName+ " " + lastName;
+
+            Console.WriteLine($"Hello {fullName}");
+        }
+    }
+}
