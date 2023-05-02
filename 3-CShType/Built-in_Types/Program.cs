21ebe1d861f468ffe01cc39c5f9f8aad61622072
fellyka <fellyka@felika.felika>
2023-05-02 04:56:45 +0200

    Built-in types

---
+++ 3-CShType/Built-in_Types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Built_in_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = true; //boolean type
+            //Integral type
+            sbyte smallDigit = 125;
+            Console.WriteLine($"sbyte value {smallDigit}");
+
+            int integer = 156;
+            Console.WriteLine($"Integer value {integer}");
+            
+            Console.WriteLine($"Minimun value of integer is {int.MinValue}\nMaximum value of integer is {int.MaxValue}");
+
+            //Float & double values
+            float floatValue = 7.236f;
+            Console.WriteLine($"Float value {floatValue}");
+
+            double doubleValue = 15.695;
+            Console.WriteLine($"Float value {doubleValue}"); //double provides more precision than float
+        }
+    }
+}
